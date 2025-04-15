a5764cd9b3bf01a1d24415315d6d165b1562b130
Osmar Gonzalez <osmarlenin.gonzalezescorche@externos-cl.cencosud.com>
2025-04-15 16:39:49 -0300

    feat: enhance Enrollment model with status type and add gocourse_domain.iml

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -7,15 +7,24 @@ import (
 )
 
 type Enrollment struct {
-	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
-	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36);"`
-	User      *User      `json:"user,omitempty"`
-	CourseID  string     `json:"course_id,omitempty" gorm:"type:char(36); not null"`
-	Course    *Course    `json:"course,omitempty"`
-	Status    string     `json:"status" gorm:"type:char(2);"`
-	CreatedAt *time.Time `json:"-"`
-	UpdatedAt *time.Time `json:"-"`
+	ID        string           `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
+	UserID    string           `json:"user_id,omitempty" gorm:"type:char(36);"`
+	User      *User            `json:"user,omitempty"`
+	CourseID  string           `json:"course_id,omitempty" gorm:"type:char(36); not null"`
+	Course    *Course          `json:"course,omitempty"`
+	Status    EnrollmentStatus `json:"status" gorm:"type:char(2);"`
+	CreatedAt *time.Time       `json:"-"`
+	UpdatedAt *time.Time       `json:"-"`
 }
+
+type EnrollmentStatus string
+
+const (
+	Pending  EnrollmentStatus = "P"
+	Active   EnrollmentStatus = "A"
+	Studying EnrollmentStatus = "S"
+	Inactive EnrollmentStatus = "I"
+)
 
 func (e *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
 
